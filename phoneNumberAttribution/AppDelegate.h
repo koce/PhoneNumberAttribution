07eac182ca179ebdda49c788dcab6c3ae7fc9ff2
赵嘉诚 <zjchen@vip.deyi.com>
2015-08-05 08:47:42 +0800

    Initial Commit

---
+++ phoneNumberAttribution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  phoneNumberAttribution
+//
+//  Created by 赵嘉诚 on 15/8/5.
+//  Copyright (c) 2015年 赵嘉诚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
