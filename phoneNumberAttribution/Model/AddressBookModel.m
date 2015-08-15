fdd84279260306771833026ae23d1d538cce1a71
赵嘉诚 <zjchen@vip.deyi.com>
2015-08-15 11:06:01 +0800

    拆分代码较长的方法

--- phoneNumberAttribution/Model/AddressBookModel.m
+++ phoneNumberAttribution/Model/AddressBookModel.m
@@ -19,6 +19,7 @@ typedef NS_ENUM(NSInteger, UpdataOption) {
     ABAddressBookRef        _adressBookRef;
     CFArrayRef              _records;
     
+    NSString*               _optionString;
     __block NSString*       _status;
     __block float           _progress;
 }
@@ -92,14 +93,13 @@ typedef NS_ENUM(NSInteger, UpdataOption) {
 
 - (void)changeAddressBookWithOption:(UpdataOption)option
 {
-    NSString *optionString;
     switch (option) {
         case UpdataOptionUpdata:{
-            optionString = @"更新";
+            _optionString = @"更新";
         }
             break;
         case UpdataOptionRestore:{
-            optionString = @"还原";
+            _optionString = @"还原";
         }
             break;
         default:
@@ -108,72 +108,78 @@ typedef NS_ENUM(NSInteger, UpdataOption) {
     
     CFIndex num = CFArrayGetCount(_records);
     NSLog(@"共有%d名联系人", (int)num);
-    _status = [NSString stringWithFormat:@"已%@%d/%d个联系人", optionString, 0, (int)num];
+    _status = [NSString stringWithFormat:@"已%@%d/%d个联系人", _optionString, 0, (int)num];
     _progress = .0;
     dispatch_async(dispatch_get_main_queue(), ^{
         [SVProgressHUD showProgress:_progress status:_status maskType:SVProgressHUDMaskTypeBlack];
     });
     
     for (int i = 0; i < num; i++) {
-        ABRecordRef person = CFArrayGetValueAtIndex(_records, i);  //获取联系人
-        ABMultiValueRef phoneNumber = ABRecordCopyValue(person, kABPersonPhoneProperty);  //获取联系人的所有电话（常量）
-        ABMutableMultiValueRef ph = ABMultiValueCreateMutableCopy(phoneNumber);   //将联系人的所有电话复制为可变对象
-        CFRelease(phoneNumber);     //release 常量的联系人所有电话
-        CFIndex phoneCount = ABMultiValueGetCount(ph); //联系人的电话数量
-        BOOL isUpdate = NO;
-        for (int j = 0; j < phoneCount; j++) {
-            CFStringRef label = ABMultiValueCopyLabelAtIndex(ph, j);   //某个电话的label
-            CFStringRef phone = ABMultiValueCopyValueAtIndex(ph, j);   //某个电话的号码
-            
-            //转换CFStringRef为NSString*，并对CFStringRef对象执行release操作（2种方法）
-            NSString *phoneString = (__bridge_transfer NSString *)phone;
-            NSString *labelStringOld = CFBridgingRelease(label);
-            
-            CFStringRef abeyanceLabel = kABHomeLabel;   //归属地label（没有create或copy，不需要管理内存）
-            
-            if (option == UpdataOptionUpdata) {  //更新通讯录
-                NSString *labelString = [self getLabelWithPhoneNumber:phoneString];
-                
-                if ([labelString isEqualToString:@""] || !labelString) {
-                    abeyanceLabel = kABHomeLabel;
-                }else{
-                    abeyanceLabel = (__bridge CFStringRef)labelString;
-                }
-                
-            }else if (option == UpdataOptionRestore){  //还原通讯录
-                abeyanceLabel = kABHomeLabel;
-            }
-            
-            NSString *labelStringNew = (__bridge NSString*)abeyanceLabel;
-            
-            if ([labelStringOld isEqualToString:labelStringNew]) {
-                isUpdate = NO;
-            }else{
-                isUpdate = YES;
-                ABMultiValueReplaceLabelAtIndex(ph, abeyanceLabel, j);   //修改电话的label
-                ABRecordSetValue(person, kABPersonPhoneProperty, ph, nil); //修改联系人的电话属性
-            }
-        }
-        if (isUpdate) {
-            ABAddressBookSave(_adressBookRef, nil); //保存通讯录
-        }
-        
-        //release
-        CFRelease(ph);
-        CFRelease(person);
-        
-        //更新UI
-        dispatch_async(dispatch_get_main_queue(), ^{
-            _status = [NSString stringWithFormat:@"已%@%d/%d个联系人", optionString, i + 1, (int)num];
-            _progress = (float)(i + 1) / (float)num;
-            [SVProgressHUD showProgress:_progress status:_status maskType:SVProgressHUDMaskTypeBlack];
-            
-            if (i == num - 1) {
-                [SVProgressHUD showSuccessWithStatus:@"已完成"];
-            }
-        });
+        [self changePersonInIndex:i Option:option TotalCount:(int)num];
     }
     CFRelease(_records);
+}
+
+//修改联系人信息
+- (void)changePersonInIndex:(int)index Option:(UpdataOption)option TotalCount:(int)num
+{
+    ABRecordRef person = CFArrayGetValueAtIndex(_records, index);  //获取联系人
+    ABMultiValueRef phoneNumber = ABRecordCopyValue(person, kABPersonPhoneProperty);  //获取联系人的所有电话（常量）
+    ABMutableMultiValueRef ph = ABMultiValueCreateMutableCopy(phoneNumber);   //将联系人的所有电话复制为可变对象
+    CFRelease(phoneNumber);     //release 常量的联系人所有电话
+    CFIndex phoneCount = ABMultiValueGetCount(ph); //联系人的电话数量
+    BOOL isUpdate = NO;
+    for (int j = 0; j < phoneCount; j++) {
+        CFStringRef label = ABMultiValueCopyLabelAtIndex(ph, j);   //某个电话的label
+        CFStringRef phone = ABMultiValueCopyValueAtIndex(ph, j);   //某个电话的号码
+        
+        //转换CFStringRef为NSString*，并对CFStringRef对象执行release操作（2种方法）
+        NSString *phoneString = (__bridge_transfer NSString *)phone;
+        NSString *labelStringOld = CFBridgingRelease(label);
+        
+        CFStringRef abeyanceLabel = kABHomeLabel;   //归属地label（没有create或copy，不需要管理内存）
+        
+        if (option == UpdataOptionUpdata) {  //更新通讯录
+            NSString *labelString = [self getLabelWithPhoneNumber:phoneString];
+            
+            if ([labelString isEqualToString:@""] || !labelString) {
+                abeyanceLabel = kABHomeLabel;
+            }else{
+                abeyanceLabel = (__bridge CFStringRef)labelString;
+            }
+            
+        }else if (option == UpdataOptionRestore){  //还原通讯录
+            abeyanceLabel = kABHomeLabel;
+        }
+        
+        NSString *labelStringNew = (__bridge NSString*)abeyanceLabel;
+        
+        if ([labelStringOld isEqualToString:labelStringNew]) {
+            isUpdate = NO;
+        }else{
+            isUpdate = YES;
+            ABMultiValueReplaceLabelAtIndex(ph, abeyanceLabel, j);   //修改电话的label
+            ABRecordSetValue(person, kABPersonPhoneProperty, ph, nil); //修改联系人的电话属性
+        }
+    }
+    if (isUpdate) {
+        ABAddressBookSave(_adressBookRef, nil); //保存通讯录
+    }
+    
+    //release
+    CFRelease(ph);
+    CFRelease(person);
+    
+    //更新UI
+    dispatch_async(dispatch_get_main_queue(), ^{
+        _status = [NSString stringWithFormat:@"已%@%d/%d个联系人", _optionString, index + 1, num];
+        _progress = (float)(index + 1) / (float)num;
+        [SVProgressHUD showProgress:_progress status:_status maskType:SVProgressHUDMaskTypeBlack];
+        
+        if (index == num - 1) {
+            [SVProgressHUD showSuccessWithStatus:@"已完成"];
+        }
+    });
 }
 
 //根据电话号码获取归属地
